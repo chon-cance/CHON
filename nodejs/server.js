ce74c79bab2a89c682de3b85652fb52f3d046e2b
daabaat <lllllls189@gmail.com>
2024-11-13 14:44:27 +0900

    project

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,28 +1,25 @@
 import express from "express";
 import cors from "cors";
 
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
 const PORT = 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5175", // React 앱의 주소
+    origin: `http://localhost:5173`,
     methods: ["GET"],
     credentials: true,
   })
 );
 
-// 날짜 형식을 ISO 문자열로 변경
-const reservedDates = [
-  new Date("2024-11-10").toISOString(),
-  new Date("2024-11-15").toISOString(),
-  new Date("2024-11-20").toISOString(),
-];
-
 app.get("/api/reserved-dates", (req, res) => {
-  res.json(reservedDates);
+  res.send(reservedDates);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port 5000`);
 });
