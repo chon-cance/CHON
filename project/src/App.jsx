b8553031dd6c7b154bf874846c0be40e58d4ab30
jeong-sunny <sunsun1993@naver.com>
2024-11-15 15:12:37 +0900

    게스트 추가

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from "./components/Footer/Footer";
+import GuestResve from "./components/GuestResve/GuestResve";
 import Header from "./components/Header/Header";
 import HostResve from "./components/HostResve/HostResve";
 import Main from "./components/Main/Main";
@@ -8,6 +9,7 @@ import "./index.css";
 export default function App() {
   return (
     <>
+      <GuestResve />
       <HostResve />
       <Header />
       <Main />
