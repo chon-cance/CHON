4c911cac48d93dc09f2367b6841db4e0c1542c22
zinee <zinee1@naver.com>
2024-11-22 10:02:49 +0900

    cors에러

--- nodejs/server.js
+++ nodejs/server.js
@@ -23,6 +23,14 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// cors에러 해결 코드
+app.use((req, res, next) => {
+  console.log("cors 에러 해결?");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // 모든 출처 허용
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // 모든 출처 허용
+  next();
+});
+
 // Solapi 라우트
 app.use("/alarm", solapiRoutes);
 
