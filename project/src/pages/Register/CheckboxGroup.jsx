251c220f418b4963a5c1cf5d397c7f008e5bc5db
daabaat <lllllls189@gmail.com>
2024-12-03 16:48:01 +0900

    checkoutgroup수정

--- project/src/pages/Register/CheckboxGroup.jsx
+++ project/src/pages/Register/CheckboxGroup.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Register.module.css";
 
-const CheckboxGroup = ({ label }) => {
+const CheckboxGroup = ({ label, checked, onChange }) => {
   return (
     <div className={styles.checkbox_group}>
       <label className={styles.checkbox_label}>
-        <input type="checkbox" />
+        <input type="checkbox" checked={checked} onChange={onChange} />
         <span className={styles.checkbox_text}>{label}</span>
       </label>
     </div>
